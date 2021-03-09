1495521690277a356ce7f78b01a278935e72f1f3
Dmitriy <grafov_dmitrii@mail.ru>
2021-03-09 18:21:28 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './Components/header/header.component';
+import { CountryComponent } from './Components/country/country.component';
+import { MainComponent } from './Components/main/main.component';
+import { WeatherItemComponent } from './Components/main/weather-item/weather-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CountryComponent,
+    MainComponent,
+    WeatherItemComponent
   ],
   imports: [
     BrowserModule,
